3d0d9ad09b1595fe44dd593295dfe550abc6a0a3
Sergey Popl <popl2017@mail.ru>
2024-05-26 17:18:59 +0300

    Добавил проект на питоне с решением

---
+++ KW.py
@@ -0,0 +1,24 @@
+massive_input = []
+massive_otput = []
+
+#Написать программу, которая из имеющегося массива строк 
+#формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести 
+#с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+while True:
+    item = input('Введите элемент массива: ')
+    if item == "end":
+        break
+    else:
+        massive_input.append(item)
+
+
+for i in range(len(massive_input)):
+    if len(massive_input[i]) <= 3:
+        massive_otput.append(massive_input[i])
+        i += 1
+    else:
+        i += 1
+
+print('Заданный массив: ', massive_input)
+print('Новый массив: ', massive_otput)
